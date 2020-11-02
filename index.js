dd352b9e6b14b8b600e2f190660e3db8783c1c56
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-11-02 16:25:35 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.length++;
+      return this.items.sort((a, b) => {  
+        if (a < b) ; 
+        return a - b;
+      });
+    }
+     
+  get(pos) {
+    if(pos > this.length || pos < 0) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    } 
+  }
 
-  get(pos) {}
+  max() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items); 
+    }
+  }
+  
+  min() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items); 
+    }
+  }
 
-  max() {}
+  sum() {
+    if (this.length <= 0) {
+      return 0;
+    } else {
+      let sum = this.items.reduce((a,b) => {
+        return a+b;
+      },0)
+      return sum;
+    }
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+     if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    } else {
+        let avg = this.sum() / this.length;
+        return avg;
+    }
+  }
 }
-
 module.exports = SortedList;
